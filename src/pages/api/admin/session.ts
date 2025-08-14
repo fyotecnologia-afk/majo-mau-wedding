446871a5b6e4b745fc21adac16221d5e23c6a266
osvaldoCesar <osva.ces.1222@gmail.com>
2025-08-13 20:01:39 -0600

    Fix: Panel Admiin v3

--- src/pages/api/admin/session.ts
+++ src/pages/api/admin/session.ts
@@ -1,10 +1,9 @@
-// /src/pages/api/admin/session.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getIronSession } from "iron-session";
-import { sessionOptions } from "../../../../lib/session";
+import { getIronSession, IronSession } from "iron-session";
+import { sessionOptions, AdminSession } from "../../../../lib/session";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getIronSession(req, res, sessionOptions);
+  const session: IronSession<AdminSession> = await getIronSession(req, res, sessionOptions);
   const isLoggedIn = Boolean(session.isLoggedIn);
   res.status(200).json({ isLoggedIn, username: session.username || null });
 }
