31c185fd64a043a2906e2f1e7692e11fbb71bb77
osvaldoCesar <osva.ces.1222@gmail.com>
2025-08-13 18:23:41 -0600

    Fix: Panel Admiin v

--- lib/session.ts
+++ lib/session.ts
@@ -1,12 +1,12 @@
 // /src/lib/session.ts
-import { IronSessionOptions } from "iron-session";
+import { SessionOptions } from "iron-session";
 
 export interface AdminSession {
   isLoggedIn?: boolean;
   username?: string;
 }
 
-export const sessionOptions: IronSessionOptions = {
+export const sessionOptions: SessionOptions = {
   cookieName: process.env.IRON_SESSION_COOKIE_NAME || "admin_session",
   password: process.env.IRON_SESSION_PASSWORD as string,
   ttl: Number(process.env.IRON_SESSION_TTL || 60 * 60 * 24), // 1 día
@@ -18,6 +18,7 @@ export const sessionOptions: IronSessionOptions = {
   },
 };
 
+// Re-declaramos la sesión para incluir AdminSession
 declare module "iron-session" {
   interface IronSessionData extends AdminSession {}
 }
